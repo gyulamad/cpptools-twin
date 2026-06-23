ff0dd9b1a41bc27563eb6b446b6fed35716193d1
Gyula Madarasz <gyula.madarasz@pm.me>
2026-06-23 09:49:27 +0100

    ncurses hack

--- tests/test_TWindow.hpp
+++ tests/test_TWindow.hpp
@@ -2,7 +2,6 @@
 
 #ifdef TEST
 
-#include <string.h>
 #include "../../misc/TEST.hpp"
 #include "../TWindow.hpp"
 
@@ -10,34 +9,22 @@
 // TWindow Tests - Focus management (requires ncurses init via newterm/delscreen)
 // ============================================================================
 
-// ============================================================================
-// TWindow Tests - Focus management (requires ncurses init via newterm/delscreen)
-// ============================================================================
-
 TEST(test_TWindow_initial_focus_is_nullptr) {
     setenv("TERM", "xterm-256color", 1);
-    FILE* devnull = fopen("/dev/null", "w");
-    SCREEN* screen = newterm("xterm-256color", devnull, stdin);
     TWindow window;
     assert(window.getFocus() == nullptr && "Initial focus should be nullptr");
-    delscreen(screen);
 }
 
 TEST(test_TWindow_setFocus_sets_and_gets_focus) {
     setenv("TERM", "xterm-256color", 1);
-    FILE* devnull = fopen("/dev/null", "w");
-    SCREEN* screen = newterm("xterm-256color", devnull, stdin);
     TWindow window;
     TBox box(10, 3, 1, "test");
     window.setFocus(&box);
     assert(window.getFocus() == &box && "getFocus should return the set focus target");
-    delscreen(screen);
 }
 
 TEST(test_TWindow_setFocus_triggers_focus_events) {
     setenv("TERM", "xterm-256color", 1);
-    FILE* devnull = fopen("/dev/null", "w");
-    SCREEN* screen = newterm("xterm-256color", devnull, stdin);
     TWindow window;
     bool focusGained = false;
     bool focusLeft = false;
@@ -59,14 +46,10 @@ TEST(test_TWindow_setFocus_triggers_focus_events) {
 
     window.setFocus(&box2);
     assert(focusLeft && "onFocusLeave should be called when focus moves away");
-
-    delscreen(screen);
 }
 
 TEST(test_TWindow_setFocus_same_box_is_noop) {
     setenv("TERM", "xterm-256color", 1);
-    FILE* devnull = fopen("/dev/null", "w");
-    SCREEN* screen = newterm("xterm-256color", devnull, stdin);
     TWindow window;
     int leaveCount = 0;
     TBox box(10, 3, 1, "test");
@@ -78,26 +61,18 @@ TEST(test_TWindow_setFocus_same_box_is_noop) {
     window.setFocus(&box);
     window.setFocus(&box); // Same box - should be a no-op
     assert(leaveCount == 0 && "Setting focus to the same box should not trigger events");
-
-    delscreen(screen);
 }
 
 TEST(test_TWindow_getRoot_returns_root_box) {
     setenv("TERM", "xterm-256color", 1);
-    FILE* devnull = fopen("/dev/null", "w");
-    SCREEN* screen = newterm("xterm-256color", devnull, stdin);
     TWindow window;
     assert(window.getRoot() != nullptr && "getRoot should return a non-null root box");
-    delscreen(screen);
 }
 
 TEST(test_TWindow_getTheme_returns_theme) {
     setenv("TERM", "xterm-256color", 1);
-    FILE* devnull = fopen("/dev/null", "w");
-    SCREEN* screen = newterm("xterm-256color", devnull, stdin);
     TWindow window;
     assert(window.getTheme() != nullptr && "getTheme should return a non-null theme");
-    delscreen(screen);
 }
 
 #endif
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,3 +1,8 @@
+#include <cstdio>
+#include <cstdlib>
+#include <termios.h>
+#include <unistd.h>
+
 #include "../../misc/TEST.hpp"
 #include "../../misc/ConsoleLogger.hpp"
 
@@ -12,9 +17,36 @@
 #include "test_TScrollbar.hpp"
 #include "test_TTheme.hpp"
 #include "test_TWindow.hpp"
+
+// ---------------------------------------------------------------
+// ncurses hack:
+
+// Global termios state saved before any ncurses initialization.
+static struct termios g_originalTermios;
+static bool g_termiosSaved = false;
+
+static void restoreTerminal() {
+    fprintf(stdout, "\n\n\n\n");
+    if (g_termiosSaved) {
+        tcsetattr(STDIN_FILENO, TCSANOW, &g_originalTermios);
+    }
+    // Send ncurses terminal reset sequences to stdout.
+    // These are the same codes endwin() sends when switching from alternate
+    // screen back to normal and restoring cursor/keyboard/mouse state.
+    fprintf(stdout, "\033[?1049l");  // Switch from alternate screen to normal
+    // fprintf(stdout, "\033[2J\033[H"); // Clear screen and move cursor home
+    fprintf(stdout, "\033[?7h");     // Re-enable line wrapping
+    fprintf(stdout, "\033[?1l");     // Exit application keypad mode (keypad off)
+    fprintf(stdout, "\033[?25h");    // Show cursor (curs_set(1))
+    fflush(stdout);
+}
 #endif // TEST
 
 int main(int argc, char** argv) {
+#ifdef TEST
+    g_termiosSaved = tcgetattr(STDIN_FILENO, &g_originalTermios) == 0;
+    atexit(restoreTerminal);
+#endif
     createLogger<ConsoleLogger>();
     Arguments args(argc, argv);
     tester.run(args);
